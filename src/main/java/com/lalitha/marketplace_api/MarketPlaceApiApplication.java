af3ab398b218a1b9ff7a97b048d9c6605b4e97cd
Lalitha Ogireddy <lalitha.ogireddy@gmail.com>
2025-02-10 09:04:58 +0100

    Initial commit

---
+++ src/main/java/com/lalitha/marketplace_api/MarketPlaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.lalitha.marketplace_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlaceApiApplication.class, args);
+	}
+
+}
